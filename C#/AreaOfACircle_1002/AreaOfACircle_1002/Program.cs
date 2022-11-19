6e23608af0bd586d49e12c780094d6a338f7932d
Denilson Bezerra <97971798+denilsonbezerra@users.noreply.github.com>
2022-11-19 18:38:29 -0300

    add: Area of a Circle 1002 C#

---
+++ C#/AreaOfACircle_1002/AreaOfACircle_1002/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class URI
+{
+
+    static void Main(string[] args)
+    {
+        // Formula to calculate the area of a circumference is defined as A = π . R2 | Considering to this problem that π = 3.14159
+        // The input contains a value of floating point (double precision), that is the variable R
+
+        double π = 3.14159;
+        double R = double.Parse(Console.ReadLine());
+        double A = Math.Pow(R, 2) * π;
+
+        // Present the message "A=" with four places after the decimal point
+
+        Console.WriteLine($"A={A:F4}");
+    }
+}
