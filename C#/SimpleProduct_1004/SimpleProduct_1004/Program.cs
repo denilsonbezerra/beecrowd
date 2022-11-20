e262ce23ab5ab4c2abe5477acaafe63ecce928d6
Denilson Bezerra <97971798+denilsonbezerra@users.noreply.github.com>
2022-11-20 16:17:56 -0300

    add: Simple Product 1004 C#

---
+++ C#/SimpleProduct_1004/SimpleProduct_1004/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class URI
+{
+
+    static void Main(string[] args)
+    {
+        // Read two integer values | Calculate the product between them and store the result in a variable
+
+        int A = int.Parse(Console.ReadLine());
+        int B = int.Parse(Console.ReadLine());
+
+        int PROD = A * B;
+
+        // Print the PROD
+
+        Console.WriteLine($"PROD = {PROD}");
+    }
+}
