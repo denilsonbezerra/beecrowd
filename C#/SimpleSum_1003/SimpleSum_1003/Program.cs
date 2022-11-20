76bc85d3f177bd9eba7d1db08aba1e163bc5a937
Denilson Bezerra <97971798+denilsonbezerra@users.noreply.github.com>
2022-11-20 15:58:43 -0300

    add: Simple Sum 1003 C#

---
+++ C#/SimpleSum_1003/SimpleSum_1003/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class URI
+{
+
+    static void Main(string[] args)
+    {
+        // Read input 2 integer numbers
+
+        int A = int.Parse(Console.ReadLine());
+        int B = int.Parse(Console.ReadLine());
+
+        // Calculate the sum between them
+
+        int SOMA = A + B;
+
+        // Write the value of sum of A and B
+
+        Console.WriteLine($"SOMA = {SOMA}");
+    }
+}
