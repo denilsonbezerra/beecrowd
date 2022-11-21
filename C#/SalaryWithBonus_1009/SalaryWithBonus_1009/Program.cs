bcc05af1a214de6a51aefd4769de3dfaaafff790
Denilson Bezerra <97971798+denilsonbezerra@users.noreply.github.com>
2022-11-21 08:33:52 -0300

    add: Salary with Bonus 1009 C#

---
+++ C#/SalaryWithBonus_1009/SalaryWithBonus_1009/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class URI
+{
+
+    static void Main(string[] args)
+    {
+        // Make a program that reads a seller's name, his/her fixed salary and the sale's total made by himself/herself in the month (in money)
+
+        // The input contains a text (employee's first name), and two double precision values, that are the seller's salary and the total value sold by him/her
+
+        string NAME = Console.ReadLine();
+        double SALARY = double.Parse(Console.ReadLine());
+        double SOLD = double.Parse(Console.ReadLine());
+
+        // Considering that this seller receives 15% over all products sold, write the final salary (total) of this seller at the end of the month, with two decimal places
+
+        double TOTAL = (SOLD * 15 / 100) + SALARY;
+        
+        Console.WriteLine($"TOTAL = R$ {TOTAL:F2}");
+    }
+}
