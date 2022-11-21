fbd7d7dfc5da582802f488c39491fe704d321ac2
Denilson Bezerra <97971798+denilsonbezerra@users.noreply.github.com>
2022-11-21 11:47:43 -0300

    add: Sphere 1011 C#

---
+++ C#/Sphere_1011/Sphere_1011/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class URI
+{
+
+    static void Main(string[] args)
+    {
+        // Make a program that calculates and shows the volume of a sphere being provided the value of its radius (R)
+        // The formula to calculate the volume is: (4/3) * pi * R3. Consider (assign) for pi the value 3.14159
+
+        double π = 3.14159;
+        double R = double.Parse(Console.ReadLine());
+        double Volume = (4.0 / 3) * π * Math.Pow(R, 3);
+
+        // The value must be presented with 3 digits after the decimal point
+
+        Console.WriteLine($"VOLUME = {Volume:F3}");
+    }
+}
