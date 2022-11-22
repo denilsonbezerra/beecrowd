c6ac2ba8ce92e5ab0e9dfca03502f72ba599c0f6
Denilson Bezerra <97971798+denilsonbezerra@users.noreply.github.com>
2022-11-22 15:15:20 -0300

    add: Consumption 1014 C#

---
+++ C#/Consumption_1014/Consumption_1014/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class URI
+{
+
+    static void Main(string[] args)
+    {
+        // Calculate a car's average consumption being provided the total distance traveled and the spent fuel total
+        // The input contains two values: one integer value X representing the total distance (in Km) and the second one is a floating point number Y  representing the spent fuel total (in liters)
+
+        int totalDistance = int.Parse(Console.ReadLine());
+        double spentFuel = double.Parse(Console.ReadLine());
+
+        // Present a value that represents the average consumption of a car with 3 digits after the decimal point
+
+        double averageConsumption = totalDistance / spentFuel;
+
+        Console.WriteLine($"{averageConsumption:F3} km/l");
+    }
+}
