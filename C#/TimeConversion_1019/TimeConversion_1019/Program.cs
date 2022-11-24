5a4aa74e9cf99a0db33bb71080bc66a0ddf71be5
Denilson Bezerra <97971798+denilsonbezerra@users.noreply.github.com>
2022-11-23 23:15:43 -0300

    add: Time Conversion 1019 C#

---
+++ C#/TimeConversion_1019/TimeConversion_1019/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+class URI
+{
+
+    static void Main(string[] args)
+    {
+
+        // Read an integer value, which is the duration in seconds of a certain event in a factory
+
+        int duration = int.Parse(Console.ReadLine());
+
+        // Inform it expressed in hours:minutes:seconds
+
+        int hours = 0;
+        int minutes = 0;
+
+        for (hours = 0; duration >= 3600; hours++)
+        {
+            duration -= 3600;
+        }
+
+        for (minutes = 0; duration >= 60; minutes++)
+        {
+            duration -= 60;
+        }
+
+        int seconds = duration;
+
+        Console.WriteLine($"{hours}:{minutes}:{seconds}");
+
+    }
+
+}
