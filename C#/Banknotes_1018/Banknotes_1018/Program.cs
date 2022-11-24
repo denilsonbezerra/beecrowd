8188e153e1ac163e8cca7af1ed1a87495577a13a
Denilson Bezerra <97971798+denilsonbezerra@users.noreply.github.com>
2022-11-23 23:14:40 -0300

    add: Banknotes 1018 C#

---
+++ C#/Banknotes_1018/Banknotes_1018/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class URI
+{
+    static void Main(string[] args)
+    {
+        // Read an integer value and calculate the smallest possible number of banknotes in which the value may be decomposed
+        // The input contains an integer value N (0 < N < 1000000)
+        // The possible banknotes are 100, 50, 20, 10, 5, 2 e 1
+
+        int number = int.Parse(Console.ReadLine());
+
+        Console.WriteLine(number);
+
+        int[] banknotes = { 100, 50, 20, 10, 5, 2, 1 };
+
+        foreach (int banknote in banknotes)
+        {
+
+            int banknoteNum = number / banknote;
+            number -= banknote * banknoteNum;
+
+            Console.WriteLine($"{banknoteNum} nota(s) de R$ {banknote},00");
+        }
+    }
+}
