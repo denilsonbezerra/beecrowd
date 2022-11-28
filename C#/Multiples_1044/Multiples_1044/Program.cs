b0a10c4f542022b95d816f5b7711ba3973a168fa
Denilson Bezerra <97971798+denilsonbezerra@users.noreply.github.com>
2022-11-27 23:50:00 -0300

    add: Multiples 1044 C#

---
+++ C#/Multiples_1044/Multiples_1044/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class URI {
+
+    static void Main(string[] args) {
+
+        // Read two integer values (A and B)
+
+        string[] values = Console.ReadLine().Split(' ');
+
+        int valueA = int.Parse(values[0]);
+        int valueB = int.Parse(values[1]);
+
+        // After, the program should print the message "Sao Multiplos" (are multiples) or "Nao sao Multiplos" (aren’t multiples), corresponding to the read values
+
+        if (valueA % valueB == 0 || valueB % valueA == 0) {
+            Console.WriteLine("Sao Multiplos");
+        } else {
+            Console.WriteLine("Nao sao Multiplos");
+        }
+
+    }
+
+}
