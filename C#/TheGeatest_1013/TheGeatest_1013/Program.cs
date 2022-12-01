f14910240878b6dd0ac3f9b0ffa38dba962f9027
Denilson Bezerra <97971798+denilsonbezerra@users.noreply.github.com>
2022-12-01 09:57:10 -0300

    update: remove comments 1013 C#

--- C#/TheGeatest_1013/TheGeatest_1013/Program.cs
+++ C#/TheGeatest_1013/TheGeatest_1013/Program.cs
@@ -5,8 +5,6 @@ class URI
 
     static void Main(string[] args)
     {
-        // Make a program that reads 3 integer values and present the greatest one followed by the message "eh o maior"
-        // Use the following formula: MaiorAB = (a + b + abs(a - b)) / 2
 
         string[] values = Console.ReadLine().Split(' ');
 
@@ -17,8 +15,8 @@ class URI
         int greatestAB = (A + B + Math.Abs(A - B)) / 2;
         int greatest = (greatestAB + C + Math.Abs(greatestAB - C)) / 2;
 
-        // Other solution is: int greatest = values.Select(int.Parse).Max();
-
         Console.WriteLine($"{greatest} eh o maior");
+        
     }
+    
 }
