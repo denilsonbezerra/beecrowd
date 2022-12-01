995ab3b6d308afd93a3927c5ed5c4bb355c31196
Denilson Bezerra <97971798+denilsonbezerra@users.noreply.github.com>
2022-12-01 18:49:59 -0300

    update: remove comments 1043 C#

--- C#/Triangle_1043/Triangle_1043/Program.cs
+++ C#/Triangle_1043/Triangle_1043/Program.cs
@@ -1,12 +1,8 @@
-﻿using System;
+using System;
 
 class URI {
 
     static void Main(string[] args) {
-
-        // Read three point floating values (A, B and C) and verify if is possible to make a triangle with them
-        // If it is possible, calculate the perimeter of the triangle and print the message: Perimetro = XX.X
-        // If it is not possible, calculate the area of the trapezium which basis A and B and C as height, and print the message: Area = XX.X
 
         string[] values = Console.ReadLine().Split(' ');
 
@@ -19,11 +15,11 @@ class URI {
             float perimeterOfTriangle = valueA + valueB + valueC;
             Console.WriteLine($"Perimetro = {perimeterOfTriangle:F1}");
 
-        } else {
-
+        } else { 
+            
             float areaOfTrapezium = ((valueA + valueB) * valueC) / 2;
             Console.WriteLine($"Area = {areaOfTrapezium:F1}");
-
+        
         }
 
     }
