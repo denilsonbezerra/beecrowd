9cbf809a43ec11f32780103a693b263fe43300d0
Denilson Bezerra <97971798+denilsonbezerra@users.noreply.github.com>
2022-12-01 09:56:00 -0300

    update: remove comments 1012 C#

--- C#/Area_1012/Area_1012/Program.cs
+++ C#/Area_1012/Area_1012/Program.cs
@@ -5,35 +5,21 @@ class URI
 
     static void Main(string[] args)
     {
-        // Make a program that reads three floating point values: A, B and C
 
         string[] values = Console.ReadLine().Split(' ');
         double A = double.Parse(values[0]);
         double B = double.Parse(values[1]);
         double C = double.Parse(values[2]);
 
-        // Then, calculate:
-        // a) the area of the rectangled triangle ((base * height) / 2) that has base A and height C
-
         double areaTriangle = (A * C) / 2;
-
-        // b) the area of the radius's circle C (π × radius²) (π = 3.14159)
 
         double areaCircle = 3.14159 * Math.Pow(C, 2);
 
-        // c) the area of the trapezium ((A + B) * C / 2) which has A and B by base, and C by height
-
         double areaTrapezium = (A + B) * C / 2;
-
-        // d) the area of ​​the square (B²) that has side B 
 
         double areaSquare = Math.Pow(B, 2);
 
-        // e) the area of the rectangle (A * B) that has sides A and B
-
         double areaRectangle = A * B;
-
-        // Show results line by line
 
         Console.WriteLine(
             $"TRIANGULO: {areaTriangle:F3}\n" +
@@ -42,5 +28,7 @@ class URI
             $"QUADRADO: {areaSquare:F3}\n" +
             $"RETANGULO: {areaRectangle:F3}"
             );
+        
     }
+    
 }
