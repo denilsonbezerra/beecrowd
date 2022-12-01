cfb81415edcb555a15dcc5d729aa17df11dbf03a
Denilson Bezerra <97971798+denilsonbezerra@users.noreply.github.com>
2022-12-01 18:05:03 -0300

    update: solution and remove comments 1038 C#

--- C#/Snack_1038/Snack_1038/Program.cs
+++ C#/Snack_1038/Snack_1038/Program.cs
@@ -1,16 +1,8 @@
-﻿using System;
+using System;
 
 class URI {
 
     static void Main(string[] args) {
-
-        // Using the following table, write a program that reads a code and the amount of an item
-        // 1 - Cachorro Quente - 4,00
-        // 2 - X-Salada - 4,50
-        // 3 - X-Bacon - 5,00
-        // 4 - Torrada Simples - 2,00
-        // 5 - Refrigerante - 1,50
-        // After, print the value to pay
 
         string[] readLine = Console.ReadLine().Split();
 
@@ -18,44 +10,25 @@ class URI {
         int itenAmount = int.Parse(readLine[1]);
 
         decimal itenPrice = 0;
-        decimal valueToPay = 0;
 
-        if (itenCode == 1) {
-
-            itenPrice = 4.00M;
-            valueToPay = itenPrice * itenAmount;
-
-            Console.WriteLine($"Total: R$ {valueToPay:F2}");
-
-        } else if (itenCode == 2) {
-
-            itenPrice = 4.50M;
-            valueToPay = itenPrice * itenAmount;
-
-            Console.WriteLine($"Total: R$ {valueToPay:F2}");
-
-        } else if (itenCode == 3) {
-
-            itenPrice = 5.00M;
-            valueToPay = itenPrice * itenAmount;
-
-            Console.WriteLine($"Total: R$ {valueToPay:F2}");
-
-        } else if (itenCode == 4) {
-
-            itenPrice = 2.00M;
-            valueToPay = itenPrice * itenAmount;
-
-            Console.WriteLine($"Total: R$ {valueToPay:F2}");
-
-        } else if (itenCode == 5) {
-
-            itenPrice = 1.50M;
-            valueToPay = itenPrice * itenAmount;
-
-            Console.WriteLine($"Total: R$ {valueToPay:F2}");
-
+        switch (itenCode) {
+            case 1:
+                itenPrice = 4.00M; break;
+            case 2:
+                itenPrice = 4.50M; break;
+            case 3:
+                itenPrice = 5.00M; break;
+            case 4:
+                itenPrice = 2.00M; break;
+            case 5:
+                itenPrice = 1.50M; break;
+            default: 
+                break;
         }
+
+        decimal valueToPay = itenPrice * itenAmount;
+
+        Console.WriteLine($"Total: R$ {valueToPay:F2}");
 
     }
 
