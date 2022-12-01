b4d97330ac0266f281252a993cc5aa4687a2cbf2
Denilson Bezerra <97971798+denilsonbezerra@users.noreply.github.com>
2022-12-01 19:18:20 -0300

    update: remove comments 1047 C#

--- C#/GameTimeWithMinutes_1047/GameTimeWithMinutes_1047/Program.cs
+++ C#/GameTimeWithMinutes_1047/GameTimeWithMinutes_1047/Program.cs
@@ -1,12 +1,8 @@
-﻿using System;
+using System;
 
 class URI {
 
     static void Main(string[] args) {
-
-        // Read the start time and end time of a game, in hours and minutes (initial hour, initial minute, final hour, final minute). Then print the duration of the game, knowing that the game can begin in a day and finish in another day,
-
-        // Obs.: With a maximum game time of 24 hours and the minimum game time of 1 minute.
 
         string[] time = Console.ReadLine().Split(' ');
 
