051e3d495dde14e816e0a52863b7a8e4fe1ce88b
Denilson Bezerra <97971798+denilsonbezerra@users.noreply.github.com>
2022-12-01 09:48:01 -0300

    update: remove comments 1007 C#

--- C#/Difference_1007/Difference_1007/Program.cs
+++ C#/Difference_1007/Difference_1007/Program.cs
@@ -5,15 +5,14 @@ class URI
 
     static void Main(string[] args)
     {
-        // Read four integer values named A, B, C and D
 
         int A = int.Parse(Console.ReadLine());
         int B = int.Parse(Console.ReadLine());
         int C = int.Parse(Console.ReadLine());
         int D = int.Parse(Console.ReadLine());
 
-        // Calculate and print the difference of product A and B by the product of C and D
-
         Console.WriteLine($"DIFERENCA = {A * B - C * D}");
+        
     }
+    
 }
