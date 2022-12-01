9e1bcd88aafe8f54b9ad2cffe499d6b5f8a13ac6
Denilson Bezerra <97971798+denilsonbezerra@users.noreply.github.com>
2022-12-01 10:01:52 -0300

    update: remove comments 1015 C#

--- C#/DistanceBetweenTwoPoints_1015/DistanceBetweenTwoPoints_1015/Program.cs
+++ C#/DistanceBetweenTwoPoints_1015/DistanceBetweenTwoPoints_1015/Program.cs
@@ -1,27 +1,23 @@
-﻿using System;
+using System;
 
 class URI
 {
 
     static void Main(string[] args)
     {
-        // Read the four values corresponding to the x and y axes of two points in the plane, p1 (x1, y1) and p2 (x2, y2) and calculate the distance between them, showing four decimal places after the comma, according to the formula
-        // Distance = Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2))
-        // The input contains two lines of data:
-        // The first one contains two double values: x1 y1
 
-        string[] firstValues = Console.ReadLine().Split();
-        double x1 = double.Parse(firstValues[0]);
-        double y1 = double.Parse(firstValues[1]);
+        string[] values1 = Console.ReadLine().Split();
+        double x1 = double.Parse(values1[0]);
+        double y1 = double.Parse(values1[1]);
 
-        // and the second one also contains two double values: x2 y2
-
-        string[] secondValues = Console.ReadLine().Split();
-        double x2 = double.Parse(secondValues[0]);
-        double y2 = double.Parse(secondValues[1]);
+        string[] values2 = Console.ReadLine().Split();
+        double x2 = double.Parse(values2[0]);
+        double y2 = double.Parse(values2[1]);
 
         double distance = Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2));
 
         Console.WriteLine($"{distance:F4}");
+
     }
+
 }
