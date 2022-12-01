06cbc941e8045ae38449845da79a75b5b8b5137b
Denilson Bezerra <97971798+denilsonbezerra@users.noreply.github.com>
2022-12-01 18:41:30 -0300

    update: remove comments 1042 C#

--- C#/SimpleSort_1042/SimpleSort_1042/Program.cs
+++ C#/SimpleSort_1042/SimpleSort_1042/Program.cs
@@ -4,9 +4,6 @@ class URI {
 
     static void Main(string[] args) {
 
-        // Read three integers and sort them in ascending order
-        // After, print these values in ascending order, a blank line and then the values in the sequence as they were readed
-
         string[] values = Console.ReadLine().Split(' ');
 
         int[] valuesInt = { int.Parse(values[0]), int.Parse(values[1]), int.Parse(values[2]) };
