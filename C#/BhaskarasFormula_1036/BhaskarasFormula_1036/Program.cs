0e801aa33891ff92c73956483fb5fad1bdabefae
Denilson Bezerra <97971798+denilsonbezerra@users.noreply.github.com>
2022-12-01 15:03:25 -0300

    update: remove comments 1036 C#

--- C#/BhaskarasFormula_1036/BhaskarasFormula_1036/Program.cs
+++ C#/BhaskarasFormula_1036/BhaskarasFormula_1036/Program.cs
@@ -1,12 +1,8 @@
-﻿using System;
+using System;
 
 class URI {
 
     static void Main(string[] args) {
-
-        // Read 3 floating-point numbers
-        // After, print the roots of bhaskara’s formula: (-B +- √(B * B - 4 * A * C)) / (2 * A)
-        // If it's impossible to calculate the roots because a division by zero or a square root of a negative number, presents the message “Impossivel calcular”
 
         string[] numbers = Console.ReadLine().Split();
 
@@ -20,15 +16,13 @@ class URI {
         if (delta < 0 || A == 0) {
             Console.WriteLine("Impossivel calcular");
         } else {
-
             double root1 = (-B + rootDelta) / (2 * A);
             double root2 = (-B - rootDelta) / (2 * A);
 
             Console.WriteLine(
                 $"R1 = {root1:F5}\n" +
                 $"R2 = {root2:F5}"
-                );
-                
+                );     
         }
 
     }
