cb0265e53db1c146101541d61c5c44380c241656
Denilson Bezerra <97971798+denilsonbezerra@users.noreply.github.com>
2022-12-01 14:40:07 -0300

    update: remove comments 1019 C#

--- C#/TimeConversion_1019/TimeConversion_1019/Program.cs
+++ C#/TimeConversion_1019/TimeConversion_1019/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 class URI
 {
@@ -6,26 +6,20 @@ class URI
     static void Main(string[] args)
     {
 
-        // Read an integer value, which is the duration in seconds of a certain event in a factory
-
-        int duration = int.Parse(Console.ReadLine());
-
-        // Inform it expressed in hours:minutes:seconds
+        int seconds = int.Parse(Console.ReadLine());
 
         int hours = 0;
         int minutes = 0;
 
-        for (hours = 0; duration >= 3600; hours++)
+        for (hours = 0; seconds >= 3600; hours++)
         {
-            duration -= 3600;
+            seconds -= 3600;
         }
 
-        for (minutes = 0; duration >= 60; minutes++)
+        for (minutes = 0; seconds >= 60; minutes++)
         {
-            duration -= 60;
+            seconds -= 60;
         }
-
-        int seconds = duration;
 
         Console.WriteLine($"{hours}:{minutes}:{seconds}");
 
