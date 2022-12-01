0b68cc39b6207000aaffa89dada3c68c6b788cf2
Denilson Bezerra <97971798+denilsonbezerra@users.noreply.github.com>
2022-12-01 09:47:13 -0300

    update: remove comments 1006 C#

--- C#/Average2_1006/Average2_1006/Program.cs
+++ C#/Average2_1006/Average2_1006/Program.cs
@@ -5,18 +5,15 @@ class URI
 
     static void Main(string[] args)
     {
-        // Read three values (variables A, B and C)
 
         double A = double.Parse(Console.ReadLine());
         double B = double.Parse(Console.ReadLine());
         double C = double.Parse(Console.ReadLine());
 
-        // Calculate the average, considering that grade A has weight 2, grade B has weight 3 and the grade C has weight 5
-
         double media = (A * 2 + B * 3 + C * 5) / 10;
 
-        // Print MEDIA
-
         Console.WriteLine($"MEDIA = {media:F1}");
+        
     }
+    
 }
