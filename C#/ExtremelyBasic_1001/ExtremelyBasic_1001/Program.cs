8d315fc012d6644d1108cc08b37e384d319a0d53
Denilson Bezerra <97971798+denilsonbezerra@users.noreply.github.com>
2022-12-01 09:41:57 -0300

    update: remove comments 1001 C#

--- C#/ExtremelyBasic_1001/ExtremelyBasic_1001/Program.cs
+++ C#/ExtremelyBasic_1001/ExtremelyBasic_1001/Program.cs
@@ -5,17 +5,14 @@ class URI
 
     static void Main(string[] args)
     {
-        // Read 2 variables, named A and B
 
         int A = int.Parse(Console.ReadLine());
         int B = int.Parse(Console.ReadLine());
 
-        // Make the sum of these two variables, assigning its result to the variable X
-
         int X = A + B;
 
-        // Print X
-
         Console.WriteLine($"X = {X}");
+        
     }
+    
 }
