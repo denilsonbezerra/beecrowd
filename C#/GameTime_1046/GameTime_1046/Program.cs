994313aba90c9491e7d86cf9b610e0ea6d18a148
Denilson Bezerra <97971798+denilsonbezerra@users.noreply.github.com>
2022-12-01 19:11:06 -0300

    update: remove comments 1046 C#

--- C#/GameTime_1046/GameTime_1046/Program.cs
+++ C#/GameTime_1046/GameTime_1046/Program.cs
@@ -1,10 +1,8 @@
-﻿using System;
+using System;
 
 class URI {
 
     static void Main(string[] args) {
-
-        // Read the start time and end time of a game, in hours
 
         string[] time = Console.ReadLine().Split(' ');
 
@@ -12,15 +10,11 @@ class URI {
         decimal endTime = decimal.Parse(time[1]);
         decimal gameTime = 0;
 
-        // Then calculate the duration of the game, knowing that the game can begin in a day and finish in another day, with a maximum duration of 24 hours
-
         if (endTime < startTime || endTime == startTime) {
             gameTime = Math.Abs(startTime - (endTime + 24));
         } else {
             gameTime = endTime - startTime;
         }
-
-        // The message must be printed in portuguese “O JOGO DUROU X HORA(S)” that means “THE GAME LASTED X HOUR(S)”
 
         Console.WriteLine($"O JOGO DUROU {gameTime} HORA(S)");
 
