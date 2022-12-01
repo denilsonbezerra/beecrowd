df32d34a35560a04198f3c50da8cd53ffe2f3a9c
Denilson Bezerra <97971798+denilsonbezerra@users.noreply.github.com>
2022-12-01 18:51:42 -0300

    update: remove comments 1044 C#

--- C#/Multiples_1044/Multiples_1044/Program.cs
+++ C#/Multiples_1044/Multiples_1044/Program.cs
@@ -1,23 +1,18 @@
-﻿using System;
+using System;
 
 class URI {
 
     static void Main(string[] args) {
-
-        // Read two integer values (A and B)
 
         string[] values = Console.ReadLine().Split(' ');
 
         int valueA = int.Parse(values[0]);
         int valueB = int.Parse(values[1]);
 
-        // After, the program should print the message "Sao Multiplos" (are multiples) or "Nao sao Multiplos" (aren’t multiples), corresponding to the read values
-
-        if (valueA % valueB == 0 || valueB % valueA == 0) {
+        if (valueA % valueB == 0 || valueB % valueA == 0) 
             Console.WriteLine("Sao Multiplos");
-        } else {
+        else
             Console.WriteLine("Nao sao Multiplos");
-        }
 
     }
 
