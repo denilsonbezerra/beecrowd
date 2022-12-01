573e6db3084c8db592cc0124b3edcdab605d2dc0
Denilson Bezerra <97971798+denilsonbezerra@users.noreply.github.com>
2022-12-01 17:52:29 -0300

    update: remove comments 1037 C#

--- C#/Interval_1037/Interval_1037/Program.cs
+++ C#/Interval_1037/Interval_1037/Program.cs
@@ -1,12 +1,8 @@
-﻿using System;
+using System;
 
 class URI {
 
     static void Main(string[] args) {
-
-        // You must make a program that read a float-point number and print a message saying in which of following intervals the number belongs:
-        // [0,25] (25,50], (50,75], (75,100]
-        // If the read number is less than zero or greather than 100, the program must print the message “Fora de intervalo” that means "Out of Interval"
 
         double number = double.Parse(Console.ReadLine());
 
