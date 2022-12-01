d9f5c30be655d2e3ad219cf82ca4bae021b40107
Denilson Bezerra <97971798+denilsonbezerra@users.noreply.github.com>
2022-12-01 09:48:55 -0300

    update: remove comments 1008 C#

--- C#/Salary_1008/Salary_1008/Program.cs
+++ C#/Salary_1008/Salary_1008/Program.cs
@@ -5,19 +5,16 @@ class URI
 
     static void Main(string[] args)
     {
-        // Write a program that reads an employee's number, his/her worked hours number in a month and the amount he received per hour. Print the employee's number and salary that he/she will receive at end of the month, with two decimal places.
-
-        // Input contains 2 integer numbers and 1 value of floating point
 
         int number = int.Parse(Console.ReadLine());
         int hours = int.Parse(Console.ReadLine());
         decimal perHour = decimal.Parse(Console.ReadLine());
 
-        // Print the number and the employee's salary
-
         Console.WriteLine(
             $"NUMBER = {number}\n" +
             $"SALARY = U$ {hours * perHour:F2}"
             );
+        
     }
+    
 }
