45fd44742550a8aba2b9d3ae26207b184ed5e97b
Denilson Bezerra <97971798+denilsonbezerra@users.noreply.github.com>
2022-12-01 09:45:14 -0300

    update: remove comments 1004 C#

--- C#/SimpleProduct_1004/SimpleProduct_1004/Program.cs
+++ C#/SimpleProduct_1004/SimpleProduct_1004/Program.cs
@@ -5,15 +5,14 @@ class URI
 
     static void Main(string[] args)
     {
-        // Read two integer values | Calculate the product between them and store the result in a variable
 
         int A = int.Parse(Console.ReadLine());
         int B = int.Parse(Console.ReadLine());
 
         int prod = A * B;
 
-        // Print the PROD
-
         Console.WriteLine($"PROD = {prod}");
+        
     }
+    
 }
