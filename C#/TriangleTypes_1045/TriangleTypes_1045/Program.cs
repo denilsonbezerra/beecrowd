563787e8d21146d568d8945af610090a03a36d95
Denilson Bezerra <97971798+denilsonbezerra@users.noreply.github.com>
2022-12-01 19:04:26 -0300

    update: remove comments 1045 C#

--- C#/TriangleTypes_1045/TriangleTypes_1045/Program.cs
+++ C#/TriangleTypes_1045/TriangleTypes_1045/Program.cs
@@ -1,14 +1,10 @@
-﻿using System;
+using System;
 
 class URI {
 
     static void Main(string[] args) {
 
-        // Read 3 double numbers (A, B and C) representing the sides of a triangle
-
         string[] values = Console.ReadLine().Split(' ');
-
-        // Arrange them in decreasing order so that the side A is the biggest of the three sides
 
         double[] valuesOrdened = { double.Parse(values[0]), double.Parse(values[1]), double.Parse(values[2]) }; 
         Array.Sort(valuesOrdened); 
@@ -18,38 +14,19 @@ class URI {
         double valueB = valuesOrdened[1];
         double valueC = valuesOrdened[2];
 
-        // Next, determine the type of triangle that they can make, based on the following cases always writing an appropriate message:
-        // if A ≥ B + C, write the message: NAO FORMA TRIANGULO
-        // if A² = B² + C², write the message: TRIANGULO RETANGULO
-        // if A² > B² + C², write the message: TRIANGULO OBTUSANGULO
-        // if A² < B² + C², write the message: TRIANGULO ACUTANGULO
-
-        if (valueA >= valueB + valueC) {
-
+        if (valueA >= valueB + valueC)
             Console.WriteLine("NAO FORMA TRIANGULO");
-
-        } else if (Math.Pow(valueA, 2) == Math.Pow(valueB, 2) + Math.Pow(valueC, 2)) {
-
+        else if (Math.Pow(valueA, 2) == Math.Pow(valueB, 2) + Math.Pow(valueC, 2))
             Console.WriteLine("TRIANGULO RETANGULO");
-
-        } else if (Math.Pow(valueA, 2) > Math.Pow(valueB, 2) + Math.Pow(valueC, 2)) {
-
+        else if (Math.Pow(valueA, 2) > Math.Pow(valueB, 2) + Math.Pow(valueC, 2))
             Console.WriteLine("TRIANGULO OBTUSANGULO");
-
-        } else {
+        else
             Console.WriteLine("TRIANGULO ACUTANGULO");
-        }
 
-        // if the three sides are the same size, write the message: TRIANGULO EQUILATERO
-        // if only two sides are the same and the third one is different, write the message: TRIANGULO ISOSCELES
-
-        if (valueA == valueB && valueB == valueC) {
-
+        if (valueA == valueB && valueB == valueC)
             Console.WriteLine("TRIANGULO EQUILATERO");
-
-        } else if ((valueA == valueB && valueB != valueC) || (valueB == valueC && valueC != valueA)) {
+        else if ((valueA == valueB && valueB != valueC) || (valueB == valueC && valueC != valueA))
             Console.WriteLine("TRIANGULO ISOSCELES");
-        } 
 
     }
 
