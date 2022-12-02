c531a036501d25a2246c26ee754391b596161075
Denilson Bezerra <97971798+denilsonbezerra@users.noreply.github.com>
2022-12-02 15:02:33 -0300

    add: Animal 1049 C#

---
+++ C#/Animal_1049/Animal_1049/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class URI {
+
+    static void Main(string[] args) {
+
+        string input1 = Console.ReadLine();
+        string input2 = Console.ReadLine();
+        string input3 = Console.ReadLine();
+
+        if (input1 == "vertebrado" && input2 == "ave" && input3 == "carnivoro") {
+            Console.WriteLine("aguia");
+        } else if (input1 == "vertebrado" && input2 == "ave" && input3 == "onivoro") {
+            Console.WriteLine("pomba");
+        } else if (input1 == "vertebrado" && input2 == "mamifero" && input3 == "onivoro") {
+            Console.WriteLine("homem");
+        } else if (input1 == "vertebrado" && input2 == "mamifero" && input3 == "herbivoro") {
+            Console.WriteLine("vaca");
+        } else if (input1 == "invertebrado" && input2 == "inseto" && input3 == "hematofago") {
+            Console.WriteLine("pulga");
+        } else if (input1 == "invertebrado" && input2 == "inseto" && input3 == "herbivoro") {
+            Console.WriteLine("lagarta");
+        } else if (input1 == "invertebrado" && input2 == "anelideo" && input3 == "hematofago") {
+            Console.WriteLine("sanguessuga");
+        } else if (input1 == "invertebrado" && input2 == "anelideo" && input3 == "onivoro") { 
+            Console.WriteLine("minhoca"); 
+        }
+
+    }
+
+}
