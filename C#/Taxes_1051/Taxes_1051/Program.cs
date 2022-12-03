3cdf5b795d7274e80f82979eb0329b30fe5fc5c8
Denilson Bezerra <97971798+denilsonbezerra@users.noreply.github.com>
2022-12-03 17:19:14 -0300

    add: Taxes 1051 C#

---
+++ C#/Taxes_1051/Taxes_1051/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+class URI {
+
+    static void Main(string[] args) {
+
+        decimal salary = decimal.Parse(Console.ReadLine());
+        decimal taxes, taxes8, taxes18, taxes28;
+
+        if (salary >= 0 && salary <= 2000) {
+            Console.WriteLine("Isento");
+        } else if (salary > 2000 && salary <= 3000) {
+
+            taxes = (salary - 2000) * 0.08m;
+            Console.WriteLine($"R$ {taxes:F2}");
+
+        } else if (salary > 3000 && salary <= 4500) {
+
+            taxes18 = salary - 3000;
+            taxes8 = salary - taxes18 - 2000;
+            taxes = taxes8 * 0.08m + taxes18 * 0.18m;
+
+            Console.WriteLine($"R$ {taxes:F2}");
+        
+        } else {
+
+            taxes28 = salary - 4500;
+            taxes18 = salary - taxes28 - 3000;
+            taxes8 = salary - taxes18 - taxes28 - 2000;
+            taxes = taxes8 * 0.08m + taxes18 * 0.18m + taxes28 * 0.28m;
+
+            Console.WriteLine($"R$ {taxes:F2}");
+
+        }
+
+    }
+
+}
