053438b69c5f53e69034cb4bcb92241b65fbd95c
Denilson Bezerra <97971798+denilsonbezerra@users.noreply.github.com>
2022-12-06 15:28:27 -0300

    add: Even Between five Numbers 1065 C#

---
+++ C#/EvenBetweenFiveNumbers_1065/EvenBetweenFiveNumbers_1065/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class URI {
+
+    static void Main(string[] args) {
+
+        int[] numbers = new int[5];
+        numbers[0] = int.Parse(Console.ReadLine());
+        numbers[1] = int.Parse(Console.ReadLine());
+        numbers[2] = int.Parse(Console.ReadLine());
+        numbers[3] = int.Parse(Console.ReadLine());
+        numbers[4] = int.Parse(Console.ReadLine());
+
+        int i = 0;
+
+        foreach (int number in numbers) {
+            if (number % 2 == 0) {
+                i++;
+            }
+        }
+
+        Console.WriteLine($"{i} valores pares");
+
+    }
+
+}
