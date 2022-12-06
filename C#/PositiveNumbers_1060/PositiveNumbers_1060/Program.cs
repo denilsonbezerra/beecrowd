0c087ddc2ceb4cc4a19743c9c567eb8fb45dde94
Denilson Bezerra <97971798+denilsonbezerra@users.noreply.github.com>
2022-12-05 22:42:59 -0300

    add: Positive Numbers 1060 C#

---
+++ C#/PositiveNumbers_1060/PositiveNumbers_1060/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class URI {
+
+    static void Main(string[] args) {
+
+        float n1 = float.Parse(Console.ReadLine());
+        float n2 = float.Parse(Console.ReadLine());
+        float n3 = float.Parse(Console.ReadLine());
+        float n4 = float.Parse(Console.ReadLine());
+        float n5 = float.Parse(Console.ReadLine());
+        float n6 = float.Parse(Console.ReadLine());
+
+        float[] numbers = { n1, n2, n3, n4, n5, n6 };
+        int i = 0;
+
+        foreach (float number in numbers) {
+            if (number > 0) {
+                i++;
+            }
+        }
+
+        Console.WriteLine($"{i} valores positivos");
+
+    }
+
+}
