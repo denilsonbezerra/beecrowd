59be3b457654e514684f22564ed253dbf7a247c4
Denilson Bezerra <97971798+denilsonbezerra@users.noreply.github.com>
2022-12-06 22:35:24 -0300

    add: Even, Odd, Positive and Negative 1066 C#

---
+++ C#/EvenOddPositiveAndNegative_1066/EvenOddPositiveAndNegative_1066/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+class URI {
+
+    static void Main(string[] args) {
+
+        int[] numbers = new int[5];
+        numbers[0] = int.Parse(Console.ReadLine());
+        numbers[1] = int.Parse(Console.ReadLine());
+        numbers[2] = int.Parse(Console.ReadLine());
+        numbers[3] = int.Parse(Console.ReadLine());
+        numbers[4] = int.Parse(Console.ReadLine());
+
+        int even = 0, odd = 0, positive = 0, negative = 0;
+
+        foreach (int number in numbers) {
+            if (number % 2 == 0) {
+                even++;
+            }
+            if (number % 2 != 0) {
+                odd++;
+            }
+            if (number > 0) {
+                positive++;
+            }
+            if (number < 0) {
+                negative++;
+            }
+        }
+
+        Console.WriteLine(
+            $"{even} valor(es) par(es)\n" +
+            $"{odd} valor(es) impar(es)\n" +
+            $"{positive} valor(es) positivo(s)\n" +
+            $"{negative} valor(es) negativo(s)"
+            );
+
+    }
+
+}
