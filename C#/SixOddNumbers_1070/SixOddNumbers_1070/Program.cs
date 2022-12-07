741ebfe37e4d7fca0970480fc309c2b4ee8f23a4
Denilson Bezerra <97971798+denilsonbezerra@users.noreply.github.com>
2022-12-06 22:38:23 -0300

    add: Six Odd Numbers 1070 C#

---
+++ C#/SixOddNumbers_1070/SixOddNumbers_1070/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class URI {
+
+    static void Main(string[] args) {
+
+        int input = int.Parse(Console.ReadLine());
+
+        for (int i = input; i < input + 12; i += 2) {
+            if (i == input && input % 2 == 0) {
+                i++;
+            }
+            Console.WriteLine(i);
+        }
+
+    }
+
+}
