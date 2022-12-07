99a65e2bde64114e60c5f5d92274c988a6472afb
Denilson Bezerra <97971798+denilsonbezerra@users.noreply.github.com>
2022-12-07 16:30:24 -0300

    update: Program.cs 1066 C#

--- C#/EvenOddPositiveAndNegative_1066/EvenOddPositiveAndNegative_1066/Program.cs
+++ C#/EvenOddPositiveAndNegative_1066/EvenOddPositiveAndNegative_1066/Program.cs
@@ -1,39 +1,28 @@
-﻿using System;
+using System;
 
 class URI {
 
     static void Main(string[] args) {
 
-        int[] numbers = new int[5];
-        numbers[0] = int.Parse(Console.ReadLine());
-        numbers[1] = int.Parse(Console.ReadLine());
-        numbers[2] = int.Parse(Console.ReadLine());
-        numbers[3] = int.Parse(Console.ReadLine());
-        numbers[4] = int.Parse(Console.ReadLine());
+        int inputNumber, even = 0, odd = 0, positive = 0, negative = 0;
 
-        int even = 0, odd = 0, positive = 0, negative = 0;
+        for (int i = 0; i < 5; i++) {
 
-        foreach (int number in numbers) {
-            if (number % 2 == 0) {
-                even++;
-            }
-            if (number % 2 != 0) {
-                odd++;
-            }
-            if (number > 0) {
-                positive++;
-            }
-            if (number < 0) {
-                negative++;
-            }
+            inputNumber = int.Parse(Console.ReadLine());
+
+            if (inputNumber % 2 == 0) even++;
+            else odd++;
+            
+            if (inputNumber > 0) positive++;
+            else if (inputNumber < 0) negative++;
+
         }
 
         Console.WriteLine(
             $"{even} valor(es) par(es)\n" +
             $"{odd} valor(es) impar(es)\n" +
             $"{positive} valor(es) positivo(s)\n" +
-            $"{negative} valor(es) negativo(s)"
-            );
+            $"{negative} valor(es) negativo(s)");
 
     }
 
