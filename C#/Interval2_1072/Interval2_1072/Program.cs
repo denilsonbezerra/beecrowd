56e2a0859fd52fe27299474d09404063ef2d5a50
Denilson Bezerra <97971798+denilsonbezerra@users.noreply.github.com>
2022-12-07 21:31:17 -0300

    add: Interval 2 1072 C#

---
+++ C#/Interval2_1072/Interval2_1072/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class URI {
+
+    static void Main(string[] args) {
+
+        int totalNumbers = int.Parse(Console.ReadLine());
+        int[] integerNumbers = new int[totalNumbers];
+
+        for (int i = 0; i < integerNumbers.Length; i++) {
+            integerNumbers[i] = int.Parse(Console.ReadLine());
+        }
+
+        int numbersIn = 0;
+        int numbersOut = 0;
+
+        foreach (int number in integerNumbers) {
+            if (number >= 10 && number <= 20) numbersIn++;
+            else numbersOut++;
+        }
+
+        Console.WriteLine(
+            $"{numbersIn} in\n" +
+            $"{numbersOut} out");
+
+    }
+
+}
