56b7d7248530c3dc3c34a9db2a7a182e7c309f10
Denilson Bezerra <97971798+denilsonbezerra@users.noreply.github.com>
2022-12-08 21:33:08 -0300

    add: Even or Odd 1074 C#

---
+++ C#/EvenOrOdd_1074/EvenOrOdd_1074/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class URI {
+
+    static void Main(string[] args) {
+
+        int totalNumbers = int.Parse(Console.ReadLine());
+        int[] integerNumbers = new int[totalNumbers];
+
+        for (int i = 0; i < integerNumbers.Length; i++) {
+            integerNumbers[i] = int.Parse(Console.ReadLine());
+        }
+
+        string numberDescription;
+
+        foreach (int number in integerNumbers) {
+
+            if (number % 2 != 0) numberDescription = "ODD ";
+            else numberDescription = "EVEN ";
+
+            if (number > 0) numberDescription += "POSITIVE";
+            else numberDescription += "NEGATIVE";
+
+            if (number == 0) numberDescription = "NULL";
+
+            Console.WriteLine(numberDescription);
+
+        }
+
+    }
+
+}
