783be51052fd554176b1ffc33eda574192f0670e
Denilson Bezerra <97971798+denilsonbezerra@users.noreply.github.com>
2022-12-08 21:44:05 -0300

    add: Multiplication Table 1078 C#

---
+++ C#/MultiplicationTable_1078/MultiplicationTable_1078/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class URI {
+
+    static void Main(string[] args) {
+
+        int input = int.Parse(Console.ReadLine());
+
+        for (int i = 1; i <= 10; i++) {
+            Console.WriteLine($"{i} x {input} = {i * input}");
+        }
+
+    }
+
+}
