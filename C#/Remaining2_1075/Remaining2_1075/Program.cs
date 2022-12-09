d32eeb0d0a4e1809fe1af0df54d6f2d330f4a755
Denilson Bezerra <97971798+denilsonbezerra@users.noreply.github.com>
2022-12-08 21:34:46 -0300

    add: Remaining 2 1075 C#

---
+++ C#/Remaining2_1075/Remaining2_1075/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class URI {
+
+    static void Main(string[] args) {
+
+        int input = int.Parse(Console.ReadLine());
+
+        for (int i = 2; i < 10000; i++) {
+            if (i % input == 2) Console.WriteLine(i);
+        }
+
+    }
+
+}
