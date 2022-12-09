eee9c0a6389bdb31af249de67f8ac47ca59f2a8a
Denilson Bezerra <97971798+denilsonbezerra@users.noreply.github.com>
2022-12-08 21:46:00 -0300

    add: Highest and Position 1080 C#

---
+++ C#/HighestAndPosition_1080/HighestAndPosition_1080/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class URI {
+
+    static void Main(string[] args) {
+
+        int[] numbers = new int[100];
+        int highestValue = numbers[0], highestPosition = 0;
+
+        for (int i = 0; i < numbers.Length ; i++) {
+
+            numbers[i] = int.Parse(Console.ReadLine());
+
+            if (numbers[i] > highestValue) {
+                highestValue = numbers[i];
+                highestPosition = i + 1;
+            }
+
+        }
+
+        Console.WriteLine($"{highestValue}\n{highestPosition}");
+
+    }
+
+}
