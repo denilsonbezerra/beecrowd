a132b1b5b3776b332e67bef9115bb349f71ab534
Denilson Bezerra <97971798+denilsonbezerra@users.noreply.github.com>
2022-12-08 21:45:08 -0300

    add: Weighted Averages 1079 C#

---
+++ C#/WeightedAverages_1079/WeightedAverages_1079/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class URI {
+
+    static void Main(string[] args) {
+
+        int numberOfTests = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i < numberOfTests; i++) {
+
+            string[] tests = Console.ReadLine().Split(' ');
+            float n1 = float.Parse(tests[0]);
+            float n2 = float.Parse(tests[1]);
+            float n3 = float.Parse(tests[2]);
+
+            float average = (n1 * 2 + n2 * 3 + n3 * 5) / 10;
+            Console.WriteLine($"{average:F1}");
+
+        }
+
+    }
+
+}
