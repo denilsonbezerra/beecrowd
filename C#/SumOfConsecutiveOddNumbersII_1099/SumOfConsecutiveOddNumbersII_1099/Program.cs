b81b08a8271fa93023d89e0f4e37490391046b8b
Denilson Bezerra <97971798+denilsonbezerra@users.noreply.github.com>
2022-12-09 22:57:52 -0300

    add: Sum of Consecutive Odd Numbers II 1099 C#

---
+++ C#/SumOfConsecutiveOddNumbersII_1099/SumOfConsecutiveOddNumbersII_1099/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class URI {
+
+    static void Main(string[] args) {
+
+        int testCases = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i < testCases; i++) {
+
+            string[] input = Console.ReadLine().Split();
+
+            int x = int.Parse(input[0]);
+            int y = int.Parse(input[1]);
+            int soma = 0;
+
+            if (x < y) {
+                for (int j = x + 1; j < y; j++)
+                    if (j % 2 != 0) soma += j;
+            } else {
+                for (int j = y + 1; j < x; j++)
+                    if (j % 2 != 0) soma += j;
+            }
+
+            Console.WriteLine(soma);
+
+        }
+
+    }
+
+}
