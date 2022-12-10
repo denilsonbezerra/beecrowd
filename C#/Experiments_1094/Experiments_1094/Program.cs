8aa7a3170707a58aaccb4692bb3eed450f8eff03
Denilson Bezerra <97971798+denilsonbezerra@users.noreply.github.com>
2022-12-09 22:52:42 -0300

    add: Experiments 1094

---
+++ C#/Experiments_1094/Experiments_1094/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+class URI {
+
+    static void Main(string[] args) {
+
+        int testCases = int.Parse(Console.ReadLine());
+        int total, amountRabbit = 0, amountRat = 0, amountFrog = 0;
+
+        for (int i = 0; i < testCases; i++) {
+
+            string[] input = Console.ReadLine().Split();
+
+            switch (input[1]) {
+                case "C": amountRabbit += int.Parse(input[0]); break;
+                case "R": amountRat += int.Parse(input[0]); break;
+                case "S": amountFrog += int.Parse(input[0]); break;
+            }
+
+        }
+
+        total = amountRabbit + amountRat + amountFrog;
+        double percentualRabbit = amountRabbit * 100.0 / total;
+        double percentualRat = amountRat * 100.0 / total;
+        double percentualFrog = amountFrog * 100.0 / total;
+
+        Console.WriteLine(
+            $"Total: {total} cobaias\n" +
+            $"Total de coelhos: {amountRabbit}\n" +
+            $"Total de ratos: {amountRat}\n" +
+            $"Total de sapos: {amountFrog}\n" +
+            $"Percentual de coelhos: {percentualRabbit:F2} %\n" +
+            $"Percentual de ratos: {percentualRat:F2} %\n" +
+            $"Percentual de sapos: {percentualFrog:F2} %");
+
+    }
+
+}
