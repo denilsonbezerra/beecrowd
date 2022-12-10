d85cc119f2ae7bea398780d7d3f3f324aceb32ad
Denilson Bezerra <97971798+denilsonbezerra@users.noreply.github.com>
2022-12-09 22:56:37 -0300

    add: Sequence IJ 3 1097 C#

---
+++ C#/SequenceIJ3_1097/SequenceIJ3_1097/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class URI {
+
+    static void Main(string[] args) {
+
+        for (int i = 1, j = 7; i <= 9 && j <= 15; j -= 1) {
+
+            Console.WriteLine($"I={i} J={j}");
+
+            if (j == i + 4) {
+                i += 2;
+                j += 5;
+            }
+        }
+    }
+}
