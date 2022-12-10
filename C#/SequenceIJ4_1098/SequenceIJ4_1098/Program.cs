cc50b7b5ad26940d9386aee67b2273f1fe7763a7
Denilson Bezerra <97971798+denilsonbezerra@users.noreply.github.com>
2022-12-09 22:57:13 -0300

    add: Sequence IJ 4 1098 C#

---
+++ C#/SequenceIJ4_1098/SequenceIJ4_1098/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class URI {
+
+    static void Main(string[] args) {
+
+        for (double i = 0, j = 1; i <= 2; j++) {
+
+            Console.WriteLine($"I={i} J={j}");
+
+            if (j == i + 3) {
+                i = Math.Round(i + 0.2, 1);
+                j = Math.Round(j - 2.8, 1);
+            }
+        }
+    }
+}
