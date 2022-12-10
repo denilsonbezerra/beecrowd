c06a1bed23ab9d81f3731802be744f1c148b13ed
Denilson Bezerra <97971798+denilsonbezerra@users.noreply.github.com>
2022-12-09 22:55:54 -0300

    add: Sequence IJ 2 1096 C#

---
+++ C#/SequenceIJ2_1096/SequenceIJ2_1096/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class URI {
+
+    static void Main(string[] args) {
+
+        for (int i = 1, j = 7; i <= 9 && j >= 5; j -= 1) {
+
+            Console.WriteLine($"I={i} J={j}");
+
+            if (j == 5) {
+                i += 2;
+                j += 3;
+            }
+        }
+    }
+}
