98420023c61c4b0d439537c50cab68d2eb15bb0b
Denilson Bezerra <97971798+denilsonbezerra@users.noreply.github.com>
2022-12-10 23:16:26 -0300

    add: Dividing X by Y 1116 C#

---
+++ C#/DividingXByY_1116/DividingXByY_1116/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class URI {
+
+    static void Main(string[] args) {
+
+        int quantityOfPairs = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i < quantityOfPairs; i++) {
+
+            string[] input = Console.ReadLine().Split();
+            double x = double.Parse(input[0]);
+            double y = double.Parse(input[1]);
+
+            if (y != 0) Console.WriteLine($"{x / y:F1}");
+            else Console.WriteLine("divisao impossivel");
+
+        }
+    }
+}
