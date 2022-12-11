05b568390bd5accccb5f00ab9729c09b4a111529
Denilson Bezerra <97971798+denilsonbezerra@users.noreply.github.com>
2022-12-10 23:31:24 -0300

    add: Score Validation 1117 C#

---
+++ C#/ScoreValidation_1117/ScoreValidation_1117/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class URI {
+
+    static void Main(string[] args) {
+
+        double average = 0;
+
+        for (int i = 0; i < 2; i++) {
+            
+            double input = double.Parse(Console.ReadLine());
+
+            if (input < 0 || input > 10) {
+                Console.WriteLine("nota invalida");
+                i--;
+            } else average += input;
+
+        }
+
+        Console.WriteLine($"media = {average / 2:F2}");
+
+    }
+
+}
