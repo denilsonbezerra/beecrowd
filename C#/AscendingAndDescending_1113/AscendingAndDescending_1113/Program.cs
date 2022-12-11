d3cbfd40fbbb771f73a962b5f8539fcfa2bfdf57
Denilson Bezerra <97971798+denilsonbezerra@users.noreply.github.com>
2022-12-10 23:12:32 -0300

    add: Ascending and Descending 1113 C#

---
+++ C#/AscendingAndDescending_1113/AscendingAndDescending_1113/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class URI {
+
+    static void Main(string[] args) {
+
+        string[] input;
+
+        do {
+
+            input = Console.ReadLine().Split();
+            int x = int.Parse(input[0]);
+            int y = int.Parse(input[1]);
+
+            if (x == y) break;
+            else if (x < y) Console.WriteLine("Crescente");
+            else Console.WriteLine("Decrescente");
+
+        } while (true);
+
+    }
+
+}
