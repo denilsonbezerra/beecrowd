cb3336b4f23cdd8bde77ee9f4699624ca4ada1b8
Denilson Bezerra <97971798+denilsonbezerra@users.noreply.github.com>
2022-12-10 23:09:43 -0300

    add: Sequence of Numbers and Sum 1101 C#

---
+++ C#/SequenceOfNumbersAndSum_1101/SequenceOfNumbersAndSum_1101/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+class URI {
+
+    static void Main(string[] args) {
+
+        string[] input;
+
+        do {
+
+            input = Console.ReadLine().Split();
+            int x = int.Parse(input[0]);
+            int y = int.Parse(input[1]);
+            int soma = 0;
+
+            if (x <= 0 || y <= 0) break;
+
+            if (x < y) {
+
+                for (int i = x; i <= y; i++) {
+                    soma += i;
+                    Console.Write($"{i} ");
+                }
+
+            } else {
+
+                for (int i = y; i <= x; i++) {
+                    soma += i;
+                    Console.Write($"{i} ");
+                }
+
+            }
+
+            Console.WriteLine($"Sum={soma}");
+            soma = 0;
+
+        } while (true);
+    }
+}
