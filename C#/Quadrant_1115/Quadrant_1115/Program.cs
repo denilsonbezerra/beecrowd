721130f0c4a97a81854d9e8b21c4451511a52635
Denilson Bezerra <97971798+denilsonbezerra@users.noreply.github.com>
2022-12-10 23:15:21 -0300

    add: Quadrant 1115 C#

---
+++ C#/Quadrant_1115/Quadrant_1115/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class URI {
+
+    static void Main(string[] args) {
+
+        while (true) {
+
+            string[] input = Console.ReadLine().Split();
+            int x = int.Parse(input[0]);
+            int y = int.Parse(input[1]);
+
+            if (x > 0 && y > 0) Console.WriteLine("primeiro"); 
+            else if (x < 0 && y > 0) Console.WriteLine("segundo"); 
+            else if (x < 0 && y < 0) Console.WriteLine("terceiro"); 
+            else if (x > 0 && y < 0) Console.WriteLine("quarto");
+            else break;
+
+        }
+
+    }
+
+}
