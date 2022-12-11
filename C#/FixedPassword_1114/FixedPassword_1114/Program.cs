416bc9fd54950b34221bd342ad8a5b7f4aab7ada
Denilson Bezerra <97971798+denilsonbezerra@users.noreply.github.com>
2022-12-10 23:14:38 -0300

    add: Fixed Password 1114 C#

---
+++ C#/FixedPassword_1114/FixedPassword_1114/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class URI {
+
+    static void Main(string[] args) {
+
+        while (true) {
+
+            int input = int.Parse(Console.ReadLine());
+
+            if (input != 2002) Console.WriteLine("Senha Invalida");
+            else { Console.WriteLine("Acesso Permitido"); break; }
+
+        }
+
+    }
+
+}
