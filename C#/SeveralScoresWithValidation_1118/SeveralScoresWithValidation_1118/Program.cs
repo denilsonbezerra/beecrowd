752a354f4e1e3cde84c975dda81b84e00035d048
Denilson Bezerra <97971798+denilsonbezerra@users.noreply.github.com>
2022-12-11 23:12:38 -0300

    add: Several Scores with Validation 1118 C#

---
+++ C#/SeveralScoresWithValidation_1118/SeveralScoresWithValidation_1118/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+class URI {
+
+    static void Main(string[] args) {
+
+        double average = 0;
+
+        for (int i = 0; i <= 2; i++) {
+
+            double input = double.Parse(Console.ReadLine());
+
+            if (input < 0 || input > 10) {
+                Console.WriteLine("nota invalida");
+                i--;
+            } else average += input;
+
+            if (i == 1) {
+
+                Console.WriteLine($"media = {average / 2:F2}");
+                average = 0;
+
+                int newCalculate = 0;
+
+                do {
+                    Console.WriteLine("novo calculo (1-sim 2-nao)");
+                    newCalculate = int.Parse(Console.ReadLine());
+                } while (newCalculate < 1 || newCalculate > 2);
+
+                if (newCalculate == 1) i -= 2;
+                else break;
+            }
+        }
+    }
+}
