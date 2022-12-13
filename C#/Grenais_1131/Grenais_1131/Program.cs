c29472d125b1be66f7b4a59d88c9ddd610c3a3af
Denilson Bezerra <97971798+denilsonbezerra@users.noreply.github.com>
2022-12-12 21:47:07 -0300

    add: Grenais 1131 C#

---
+++ C#/Grenais_1131/Grenais_1131/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+class URI {
+
+    static void Main(string[] args) {
+
+        int nGrenal = 0, interWins = 0, gremioWins = 0, draws = 0;
+
+        for (;;) {
+
+            nGrenal++;
+
+            string[] input = Console.ReadLine().Split();
+            int interGoals = int.Parse(input[0]), gremioGoals = int.Parse(input[1]);
+
+            if (interGoals > gremioGoals) interWins++;
+            else if (gremioGoals > interGoals) gremioWins++;
+            else draws++;
+
+            int option;
+
+            do {
+                Console.WriteLine("Novo grenal (1-sim 2-nao)");
+                option = int.Parse(Console.ReadLine());
+            } while (option != 1 && option != 2);
+
+            if (option == 2) break;
+
+        }
+
+        Console.WriteLine(
+            $"{nGrenal} grenais\n" +
+            $"Inter:{interWins}\n" +
+            $"Gremio:{gremioWins}\n" +
+            $"Empates:{draws}");
+
+        if (interWins > gremioWins) Console.WriteLine("Inter venceu mais");
+        else if (gremioWins > interWins) Console.WriteLine("Gremio venceu mais");
+        else Console.WriteLine("Não houve vencedor");
+
+    }
+
+}
