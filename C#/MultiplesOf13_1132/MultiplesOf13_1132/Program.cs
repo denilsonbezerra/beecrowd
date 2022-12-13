f3b35ce793c8f544c3c0d0e84b80e676960ae74b
Denilson Bezerra <97971798+denilsonbezerra@users.noreply.github.com>
2022-12-13 17:20:20 -0300

    add: Multiples of 13 1132 C#

---
+++ C#/MultiplesOf13_1132/MultiplesOf13_1132/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class URI {
+
+    static void Main(string[] args) {
+
+        int x = int.Parse(Console.ReadLine());
+        int y = int.Parse(Console.ReadLine());
+        int sum = 0;
+
+        if (x > y) {
+            int aux = y;
+            y = x;
+            x = aux;
+        }
+
+        for (int i = x; i <= y; i++) {
+            if (i % 13 != 0) {
+                sum += i;
+            }
+        }
+
+        Console.WriteLine(sum);
+
+    }
+
+}
