ecb539b3e363e45b25231901bf42bc65cb52d992
Denilson Bezerra <97971798+denilsonbezerra@users.noreply.github.com>
2022-12-14 19:27:22 -0300

    add: Rest of a Division 1133 C#

---
+++ C#/RestOfADivision_1133/RestOfADivision_1133/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class URI {
+
+    static void Main(string[] args) {
+
+        int start = int.Parse(Console.ReadLine()), final = int.Parse(Console.ReadLine());
+
+        if (start > final) {
+            int aux = final;
+            final = start;
+            start = aux;
+        }
+
+        for (int i = start + 1; i < final; i++) {
+            if (i % 5 == 2 || i % 5 == 3) Console.WriteLine(i);
+        }
+
+    }
+}
