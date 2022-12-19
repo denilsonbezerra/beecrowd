00508e2b516fd5c9675f22fbf9dcdd603fbbd31b
Denilson Bezerra <97971798+denilsonbezerra@users.noreply.github.com>
2022-12-18 22:54:07 -0300

    add: Type of Fuel 1134 C#

---
+++ C#/TypeOfFuel_1134/TypeOfFuel_1134/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+class URI {
+
+    static void Main(string[] args) {
+
+        int fuelOption = 0;
+        int alcohol = 0;
+        int gasoline = 0;
+        int diesel = 0;
+
+        do {
+
+            fuelOption = int.Parse(Console.ReadLine());
+
+            switch(fuelOption) {
+                case 1:
+                    alcohol++; break;
+                case 2:
+                    gasoline++; break;
+                case 3:
+                    diesel++; break;
+                default: break;
+            }
+
+        } while(fuelOption != 4);
+
+        Console.WriteLine(
+            $"MUITO OBRIGADO\n" +
+            $"Alcool: {alcohol}\n" +
+            $"Gasolina: {gasoline}\n" +
+            $"Diesel: {diesel}");
+
+    }
+
+}
