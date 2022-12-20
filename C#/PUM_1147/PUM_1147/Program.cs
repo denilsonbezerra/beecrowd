2d7ce92d2c2aa300444f4f174b6296693b88f590
Denilson Bezerra <97971798+denilsonbezerra@users.noreply.github.com>
2022-12-19 22:37:52 -0300

    add: PUM 1147 C#

---
+++ C#/PUM_1147/PUM_1147/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class URI {
+
+    static void Main(string[] args) {
+
+        int input = int.Parse(Console.ReadLine());
+
+        for (int i = 1, j = 1; i <= input; i ++, j+= 4) {
+            Console.WriteLine($"{j} {j + 1} {j + 2} PUM");
+        }
+
+    }
+
+}
