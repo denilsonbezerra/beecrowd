5adf30fbb8d16f6363a27ff1a7b7471e4b4a7b00
Denilson Bezerra <97971798+denilsonbezerra@users.noreply.github.com>
2022-12-20 23:01:48 -0300

    add: Squared and Cubic 1143 C#

---
+++ C#/SquaredAndCubic__1143/SquaredAndCubic__1143/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class URI {
+
+    static void Main(string[] args) {
+
+        int input = int.Parse(Console.ReadLine());
+
+        for (int i = 1; i <= input; i++) {
+            Console.WriteLine($"{i} {i * i} {i * i * i}");
+        }
+
+    }
+
+}
