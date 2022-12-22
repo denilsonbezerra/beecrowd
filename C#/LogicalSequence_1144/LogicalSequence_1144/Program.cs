49d830fd5c80f5cf51ba5540a49f14f4c6fa69db
Denilson Bezerra <97971798+denilsonbezerra@users.noreply.github.com>
2022-12-21 22:29:48 -0300

    add: Logical Sequence 1144 C#

---
+++ C#/LogicalSequence_1144/LogicalSequence_1144/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class URI {
+
+    static void Main(string[] args) {
+
+        int input = int.Parse(Console.ReadLine());
+
+        for (int i = 1; i <= input; i++) {
+
+            Console.WriteLine(
+                $"{i} {i * i} {i * i * i}\n" +
+                $"{i} {i * i + 1} {i * i * i + 1}");
+
+        }
+
+    }
+
+}
