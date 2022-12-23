ef371ae01139e6e8beea63e7e8376c6b9aca1323
Denilson Bezerra <97971798+denilsonbezerra@users.noreply.github.com>
2022-12-23 15:53:03 -0300

    add: Growing Sequences 1146 C#

---
+++ C#/GrowingSequences_1146/GrowingSequences_1146/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class URI {
+
+    static void Main(string[] args) {
+
+        for (;;) {
+
+            int input = int.Parse(Console.ReadLine()); 
+
+            if (input == 0) break;
+
+            for (int i = 1; i <= input; i++) {
+                if (i < input) Console.Write($"{i} ");
+                else Console.WriteLine(i);
+            }
+
+        }
+
+    }
+
+}
