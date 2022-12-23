8a83059cfb8dc003d44ec6c0f21c1410e2af9cc3
Denilson Bezerra <97971798+denilsonbezerra@users.noreply.github.com>
2022-12-22 22:27:47 -0300

    add: Logical Sequence 2 1145 C#

---
+++ C#/LogicalSequence2_1145/LogicalSequence2_1145/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class URI {
+
+    static void Main(string[] args) {
+
+        string[] input = Console.ReadLine().Split();
+        int x = int.Parse(input[0]);
+        int y = int.Parse(input[1]);
+
+        for (int i = 1; i <= y; i++) {
+
+            if (i % x != 0) Console.Write($"{i} ");
+            else Console.WriteLine($"{i}");
+
+        }
+
+    }
+
+}
